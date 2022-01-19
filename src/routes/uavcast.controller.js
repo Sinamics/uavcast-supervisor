3bc178d89fd09791a4553a2e98acef3cba64c7aa
Bernt Christian Egeland <cbegelan@gmail.com>
2022-01-19 13:08:33 +0000

    re-named

--- src/routes/uavcast.controller.js
+++ src/routes/uavcast.controller.js
@@ -4,15 +4,15 @@ class UavcastController {
   constructor() {}
   startContainer(version, callback) {
     const uavcastInstallUrl = `
-    echo "Restarting UAVcast-Pro...\n" && docker stop uavcast && \ 
+    echo "Restarting uavcast...\n" && docker stop uavcast && \ 
     echo "Deleting previous version\n" && docker rm uavcast && \ 
-    echo "Starting ${version} UAVcast-Pro container...\n" && docker run \
+    echo "Starting ${version} uavcast container...\n" && docker run \
     --restart unless-stopped --name uavcast -d \
     -v uavdata:/home/uavcast/persistent \
     -v /var/lib/zerotier-one:/var/lib/zerotier-one \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /dev:/dev \
-    --privileged=true --net=host sinamics/uavcast-pro:${version}
+    --privileged=true --net=host sinamics/uavcast:${version}
     `;
 
     // const uavcastInstallUrl = `curl -s http://install.uavmatrix.com/next/ | sudo bash -s -- -v ${version} -s`;
@@ -25,7 +25,7 @@ class UavcastController {
     });
   }
   downloadNewVersion(version, callback) {
-    const child = spawn(`docker pull sinamics/uavcast-pro:${version}`, { shell: true });
+    const child = spawn(`docker pull sinamics/uavcast:${version}`, { shell: true });
     child.stdout.on('data', async (data) => {
       // console.log(data.toString('utf8'));
       // socket.emit('S_SUPERVISOR_MESSAGES', data.toString('utf8'));
